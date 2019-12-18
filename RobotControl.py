8aa66566a2eed6096ee2aafc14965bcf1fb66451
diyogon <diyogon@gmail.com>
2019-12-18 13:38:37 -0500

    Consolidated ard and gant control

---
+++ RobotControl.py
@@ -0,0 +1,155 @@
+import cv2
+import mk2Camera
+from time import sleep
+from GantryControl import Gantry
+
+xCenter = 240
+yCenter = 320
+pixelsToMM = (100.0/9)
+TRAYS = 0
+FILTERS = 1
+
+class robotControl():
+  def __init__(self, mode = TRAYS):
+    self.gant = Gantry()
+    self.cap = cv2.VideoCapture(1)
+    self.mode = mode
+    if TRAYS == mode:
+      self.maxTraySize = 8
+      self.xStart = 75
+      self.yStart = 145
+    elif FILTERS == mode:
+      self.maxTraySize = 14
+      self.xStart = 85
+      self.yStart = 333
+    else:
+      self.maxTraySize = 8
+      self.xStart = 75
+      self.yStart = 145
+      print("ERROR: Unknown mode")
+    self.setToStart()
+
+  def home(self):
+    self.gant.home()
+    
+  def capture(self):
+    self.samples += 1
+    l, p = readLabels(1, self.x, self.y, self.gant, self.cap)
+    self.advance()
+    if self.samples >= self.maxTraySize:
+      self.setToStart()
+    return l, p
+
+  def advance(self):
+    if self.mode == TRAYS:
+      self.y += 65.3
+    elif self.mode == FILTERS:
+      self.advanceFilters()
+
+  def advanceFilters(self):
+    columnPosition = self.samples % 2
+    if columnPosition < 1:
+      #Move to next column
+      self.x = self.xStart
+      self.y += 45
+    else:
+      self.x -= 50
+
+  def sendTo(self, x, y):
+    self.gant.sendTo(str(x), str(y))
+
+  def checkMoving(self):
+    return self.gant.checkMoving()
+
+  def setToStart(self):
+    self.x = self.xStart
+    self.y = self.yStart
+    self.samples = 0
+
+  def close(self):
+    self.cap.release()
+
+def tryToFindLabel(cap, t):
+  i = 0
+  label = ""
+  while i < t:
+    ret, frame = cap.read()
+    processed, label = mk2Camera.processFrame(frame)
+    if "" != label:
+      print("Found label")
+      i = t
+    cv2.imshow('processView',processed)
+    cv2.waitKey(1)
+    i+=1
+  return label
+
+def tryToFindTape(number, x, y, cap):
+  i = 0
+  while i < number:
+    ret, frame = cap.read()
+    processed, center = mk2Camera.processColor(frame)
+    #cv2.imshow('processView',processed)
+    #cv2.waitKey(1)
+    if center is not None:
+      break
+    i+=1
+  if center is not None:
+    xTarget = (center[1] - xCenter)/pixelsToMM
+    yTarget = (center[0] - yCenter)/pixelsToMM
+    return (str.format("%4.3f"%(x+xTarget)), str.format("%4.3f"%(y+yTarget)))
+  else:
+    print("Could not find tape")
+    return None
+
+def readLabels(number, x, y, gant, cap):
+  #Tray Settings
+  #yOffset = 65.3
+  #xOffset = 25
+  #Filter Settings
+  yOffset = 40
+  xOffset = 0
+  labels = []
+  positions = []
+  repeat = True
+  for n in range(number):
+    yTarget = y+(n*yOffset)
+    gant.sendTo(str.format("%4.3f"%(x)), str.format("%4.3f"%(yTarget)))
+    labels.append(tryToFindLabel(cap, 0))
+    cX = x-(1*xOffset)
+    gant.sendTo(str.format("%4.3f"%(cX)), str.format("%4.3f"%(yTarget)))
+    center = tryToFindTape(20, cX, yTarget, cap)
+    if center is None and repeat:
+      cX = x-(1*xOffset)
+      gant.sendTo(str.format("%4.3f"%(cX)), str.format("%4.3f"%(yTarget)))
+      center = tryToFindTape(20, cX, yTarget, cap)
+      if center is None:
+        cX = x-(1*xOffset)
+        gant.sendTo(str.format("%4.3f"%(cX)), str.format("%4.3f"%(yTarget)))
+        center = tryToFindTape(20, cX, yTarget, cap)
+    positions.append(center)
+  return labels, positions
+
+def singlePass(number, gant):
+  cap = cv2.VideoCapture(1)
+  labels, positions = readLabels(number, 62.5, 205, gant, cap)
+  cap.release()
+  return(labels, positions)
+
+
+if __name__ == '__main__':
+  gant = Gantry()
+  labels, positions = singlePass(1, gant)
+  print(labels)
+  print(positions)
+  gant.sendTo(str(0),str(0))
+  gant.close()
+  
+'''
+  while(True):
+    ret, frame = cap.read()
+    processed = mk2Camera.processFrame(frame)
+    cv2.imshow('frame',processed)
+    if cv2.waitKey() & 0xFF == ord('q'):
+        break
+  
+  cv2.destroyAllWindows()'''
--- UnifiedRun.py
+++ UnifiedRun.py
@@ -1,46 +1,38 @@
-import ArduinoControl
+import RobotControl
 import XRFControl
-from GantryControl import Gantry
 import cv2
 from time import sleep, time
 
-#Tray Settings
-#XRF_X_OFFSET = 27
-#XRF_Y_OFFSET = 30
-#TRAY_SIZE = 8
-#Filter Settings
-XRF_X_OFFSET = -24.23
-XRF_Y_OFFSET = -35.19
-TRAY_SIZE = 14
-SAMPLES_TO_READ = 14*1
 DEBUG = False
+TRAYS = 0
+FILTERS = 1
 
-def mainLoop():
+def mainLoop(mode = FILTERS, samples = (14*2)):
   start_time = time()
-  gant = Gantry()
-  ard = ArduinoControl.arduinoControl(gant)
+  (xrfXOffset, xrfYOffset, traySize) = getSettings(mode = mode)
+  robot = RobotControl.robotControl(mode = mode)
   if askToHome():
-    gant.home()
+    robot.home()
   mXRF = XRFControl.XRF()
   i = 0
-  while i < SAMPLES_TO_READ:
-    if (i != 0) and (((i) % TRAY_SIZE) == 0):
-      gant.sendTo(str(0),str(0))
+  while i < samples:
+    if (i != 0) and (((i) % traySize) == 0):
+      robot.sendTo(0, 0)
       cTime = time()
       elapsed = cTime - start_time
       start_time = cTime
       print("Time: %s seconds, %s minutes" % ((elapsed), (elapsed/60.0)))
       #input("Tray done! Reload tray and press enter ")
-    label, position = ard.capture()
-    targetLabel = correctLabels(label, i)
-    targetPosition = correctPositions(position)
+    label, position = robot.capture()
+    targetLabel = correctLabels(label, i, traySize)
+    targetPosition = correctPositions(position, xrfXOffset, xrfYOffset)
     if (targetPosition is not None):# and (not mXRF.error):
       if DEBUG:
         print(position)
         print(targetPosition)
         print(targetLabel)
-      gant.sendTo(targetPosition[0][0], targetPosition[0][1])
-      while gant.checkMoving():
+      robot.sendTo(targetPosition[0][0], targetPosition[0][1])
+      while robot.checkMoving():
         sleep(1)
       success = mXRF.sample(targetLabel)
       if success:
@@ -50,14 +42,13 @@ def mainLoop():
         input("Please manually run XRF and hit enter")
         mXRF.reset()
     i += 1
-  ard.close()
   if not DEBUG:
-    gant.sendTo(str(0),str(0))
+    robot.sendTo(str(0),str(0))
     cTime = time()
     elapsed = cTime - start_time
     start_time = cTime
     print("Time: %s seconds, %s minutes" % ((elapsed), (elapsed/60.0)))
-  gant.close()
+  robot.close()
 
 def askToHome():
   yes = input("Home AXLE before start? (y/n): ")
@@ -66,24 +57,33 @@ def askToHome():
     yes = input("Enter exactly y or n: ")
   return yes == 'y'
 
-def correctLabels(labels, i):
+def correctLabels(labels, i, traySize):
   '''ELEPHANT- Label song and dance goes here'''
   ret = "Tray "
-  ret += str(int((int(i)/int(TRAY_SIZE))+int(1)))
+  ret += str(int((int(i)/int(traySize))+int(1)))
   ret += " Filter "
-  ret += str((i % TRAY_SIZE)+1)
+  ret += str((i % traySize)+1)
   return ret
 
-def correctPositions(positions):
+def correctPositions(positions, xrfXOffset, xrfYOffset):
   ret = []
   for position in positions:
     if position is not None:
-      p0 = max(float(position[0]) + XRF_X_OFFSET, 0);
-      p1 = float(position[1]) + XRF_Y_OFFSET;
+      p0 = max(float(position[0]) + xrfXOffset, 0);
+      p1 = float(position[1]) + xrfYOffset;
       ret.append([str.format("%4.3f"%(p0)), str.format("%4.3f"%(p1))])
     else:
       ret.append(None)
   return ret
+
+def getSettings(mode):
+  if TRAYS == mode:
+    return (27, 30, 8)
+  elif FILTERS == mode:
+    return (-24.23, -35.19, 14)
+  else:
+    print("ERROR: Unknown mode")
+    return (0, 0, 0)
 
 if __name__ == '__main__':
   mainLoop()
